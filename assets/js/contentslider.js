6be3274381206b4e802bcc49e56667644b24a621
xp.li88@gmail.com <xp.li88@gmail.com>
2015-05-13 22:54:30 +0800

    formwizard

---
+++ assets/js/contentslider.js
@@ -0,0 +1,18 @@
+define('contentslider', ['main'], function(G) {
+
+    var p = {};
+
+    p.init = function() {
+        $('.modal.aside').ace_aside();
+        
+        $('#aside-inside-modal').addClass('aside').ace_aside({container: '#my-modal > .modal-dialog'});
+        
+        $(document).one('ajaxloadstart.page', function(e) {
+            //in ajax mode, remove before leaving page
+            $('.modal.aside').remove();
+            $(window).off('.aside')
+        });
+    };
+
+    return p;
+});
